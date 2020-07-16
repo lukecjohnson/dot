2b5c97772826ef5a8c82e351cd5efca633812768
Luke Johnson <luke.johnson9423@gmail.com>
2020-07-15 20:15:11 -0400

    add argument parsing for input/output directories

--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log('Hello, world!');
+import { promises as fs } from 'fs';
+import * as path from 'path';
+
+async function compileEntry(entry: string, root: string, output: string): Promise<void> {
+  const html = await fs.readFile(path.join(root, entry), { encoding: 'utf-8' });
+
+  await fs.mkdir(output, { recursive: true });
+
+  await fs.writeFile(path.join(output, entry), html, 'utf-8');
+}
+
+async function main(): Promise<void> {
+  const args = process.argv0 === 'node' 
+    ? process.argv.slice(2)
+    : process.argv.slice(1);
+
+  const root = path.join(process.cwd(), args[0]);
+  const output = path.join(process.cwd(), args[1]);
+
+  const entries = (await fs.readdir(root)).filter(f => path.extname(f) === '.html');
+  for (const entry of entries) {
+    await compileEntry(entry, root, output);
+  }
+} 
+
+main();
