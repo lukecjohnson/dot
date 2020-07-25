d2f30aa4b090ed505c98f62628482b0eab5795f4
Luke Johnson <luke.johnson9423@gmail.com>
2020-07-25 15:39:19 -0400

    allow space before angle bracket in opening component tag

--- index.ts
+++ index.ts
@@ -29,7 +29,7 @@ const dir = {
 
 const patterns = {
   whitespace: /^\s+|\s+$/g,
-  component: /<component:([a-z][a-z-]*)((?:\s+[a-z][a-z0-9-]*="[^"]*")*)(?:\s*\/>|>(?!.*<component)(.*?)<\/component:\1>)/gms,
+  component: /<component:([a-z][a-z-]*)((?:\s+[a-z][a-z0-9-]*="[^"]*")*)\s*(?:\/>|>(?!.*<component)(.*?)<\/component:\1>)/gms,
   slot: /<slot(?:\s*\/>|>(.*?)<\/slot>)/gms,
 };
 
